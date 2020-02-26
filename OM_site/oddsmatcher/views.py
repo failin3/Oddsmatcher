a90b153bf897cee7a630957527a2f847bf4f85d9
failin3 <felix.hoens@gmail.com>
2020-02-26 21:40:41 +0100

    Created login view

--- OM_site/oddsmatcher/views.py
+++ OM_site/oddsmatcher/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
 
 import json
 
-
+@login_required(login_url='/login') #if not logged in redirect to /
 def index(request):
     with open("site_input.json") as f:
         runner_data = json.load(f)
@@ -11,3 +16,16 @@ def index(request):
         "runner_data" : runner_data
     }
     return render(request, 'oddsmatcher/index.html', context)
+
+def auth_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect('/accounts/invalid')
+    else:
+        return render(request, "oddsmatcher/login.html")
