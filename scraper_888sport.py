e75b2fa18e25290fd19d3bc0d2ea8e424ed72a92
failin3 <felix.hoens@gmail.com>
2020-07-08 22:21:07 +0200

    Now does more leagues and fixed randomizer bug

--- scraper_888sport.py
+++ scraper_888sport.py
@@ -19,7 +19,12 @@ def get888sportData():
     driver = webdriver.Chrome("bin/chromedriver")
     driver.get("https://www.888sport.com/#/filter/football")
     sleep(10)
-    webdriver.ActionChains(driver).key_down(u'\ue00d').perform()
+    #webdriver.ActionChains(driver).key_down(u'\ue00d').perform()
+    driver.find_element_by_class_name("close").click()
+    countries = driver.find_elements_by_class_name("KambiBC-collapsible-container")[2:7]
+    for country in countries:
+        country.click()
+        sleep(1)
     soup = BeautifulSoup(driver.page_source, features="html.parser")
     game_list = []
     for element in soup.find_all("a", class_="KambiBC-event-item__link"):
@@ -29,12 +34,14 @@ def get888sportData():
 
         #0 selects outright, otherwise you get second bet type as well
         odds = element.find_all("div", class_="KambiBC-bet-offer__outcomes")[0]
-        r1 = odds.find_all("div", class_="sc-AxheI cWjOQp")[0].text
-        rX = odds.find_all("div", class_="sc-AxheI cWjOQp")[1].text
-        r2 = odds.find_all("div", class_="sc-AxheI cWjOQp")[2].text
+        r1 = odds.find_all("button", class_="KambiBC-betty-outcome")[0].find_all("div")[5].text
+        rX = odds.find_all("button", class_="KambiBC-betty-outcome")[1].find_all("div")[5].text
+        r2 = odds.find_all("button", class_="KambiBC-betty-outcome")[2].find_all("div")[5].text
         game_list.append(OutrightGame("{} - {}".format(team1, team2), r1, rX, r2))
 
     return game_list
 
 if __name__ == "__main__":
-    pass
+    game_list = get888sportData()
+    for game in game_list:
+        print("{:<40} {:>7} {:>7}  {:>7}".format(game.name, game.r1, game.rX, game.r2))
