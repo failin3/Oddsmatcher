6f8f3b9c3c8e1aca43533a6f3b6066a2b0c82e77
failin3 <felix.hoens@gmail.com>
2020-07-22 11:54:40 +0000

    Close chromium when the python script crashes

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 until nohup python3 src/main.py -p > log.txt 2>&1; do
     echo "Server 'Oddsmatcher' crashed with exit code $?.  Respawning.." >&2
     sleep 1
+    pkill -o chromium
+    sleep 1
 done
