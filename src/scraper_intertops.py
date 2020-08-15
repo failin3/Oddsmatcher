e02dc57441b1a92ad62e50271b1925a92e848b18
failin3 <felix.hoens@gmail.com>
2020-08-15 12:48:52 +0200

    Added try/except bet90 and added into oddsmatcher-dev

--- src/scraper_intertops.py
+++ src/scraper_intertops.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from time import sleep
 
+from logger_manager import *
+
 
 class OutrightGame:
     def __init__(self, name, r1, rX, r2):
@@ -13,28 +15,32 @@ class OutrightGame:
 
 def getDataFromPage(soup):
     game_list = []
-    
     for list_item in soup.find_all("li"):
-        #End of games
         try:
-            if list_item.has_attr("class") and list_item["class"][0] == "load-more-container":
-                break
-        except IndexError:
-            pass
-        team_names = list_item.find("a").text.strip()
-        #Sometimes extra marketing info is like last minute or top bet is on extra lines, remove those
-        team_names = team_names.split("\n")
-        if isinstance(team_names, list):
-            team_names = team_names[0]
-        team1 = team_names.split(" v ")[0]
-        team2 = team_names.split(" v ")[1]
-        
-        r1 = list_item.find_all("span", "fright odds")[0].text
-        rX = list_item.find_all("span", "fright odds")[1].text
-        r2 = list_item.find_all("span", "fright odds")[2].text
+            #End of games
+            try:
+                if list_item.has_attr("class") and list_item["class"][0] == "load-more-container":
+                    break
+            except IndexError:
+                pass
+            team_names = list_item.find("a").text.strip()
+            #Sometimes extra marketing info is like last minute or top bet is on extra lines, remove those
+            team_names = team_names.split("\n")
+            if isinstance(team_names, list):
+                team_names = team_names[0]
+            team1 = team_names.split(" v ")[0]
+            team2 = team_names.split(" v ")[1]
 
-        game = OutrightGame("{} vs {}".format(team1, team2), r1, rX, r2)   
-        game_list.append(game)
+            odds_location = list_item.find_all("span", "fright odds")
+            
+            r1 = odds_location[0].text
+            rX = odds_location[1].text
+            r2 = odds_location[2].text
+
+            game = OutrightGame("{} vs {}".format(team1, team2), r1, rX, r2)   
+            game_list.append(game)
+        except Exception as e:
+            logger.debug(e)
     return game_list
 
 
