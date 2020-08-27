3e035fa10f5b92bc65be86fc26458214f377195a
failin3 <felix.hoens@gmail.com>
2020-08-27 12:51:32 +0200

    Added schedule prompt

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 pkill chromium
-until nohup python3 src/main.py -p > log.txt 2>&1; do
+read -p 'Schedule to run: ' schedule
+until nohup python3 src/main.py -p --schedule $schedule > log.txt 2>&1; do
     echo "Server 'Oddsmatcher' crashed with exit code $?.  Respawning.." >&2
     sleep 1
     pkill -P $$
