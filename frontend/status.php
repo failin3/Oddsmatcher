50886b90054d51465e78c9f3cf7a78096ba676bf
failin3 <felix.hoens@gmail.com>
2020-08-28 20:37:53 +0200

    Made page easier on the eyes

--- frontend/status.php
+++ frontend/status.php
@@ -3,8 +3,9 @@ $servername = "localhost";
 $username = "u80189p74860_oddsmatcher";
 $password = "Kq90*r%XXlEXaUIvoxwo";
 
-$green_time = 5;
-$orange_time = 10;
+$green_time = 7*60;
+$orange_time = 15*60;
+$timeout_time = 10000*60;
 
 
 // Create connection
@@ -18,33 +19,59 @@ $query = "SELECT TABLE_NAME, UPDATE_TIME FROM information_schema.tables WHERE TA
 $results=mysqli_query($mysqli,$query);
 $row_count=mysqli_num_rows($results);
 ?>
+<h1 style="text-align: center; font-family: arial, sans-serif;">Status</h1>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+}
 
-<table>
+td, th {
+  border: 1px solid #dddddd;
+  text-align: center;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+<table style="margin-left: auto; margin-right: auto;">
     <tr>
         <th>Table Name</th>
         <th>Time Last Update</th>
-        <th>Status</th>
+        <th>Betfair</th>
+        <th>Matchbook</th>
     </tr>
 
 <?php
 while ($row = mysqli_fetch_array($results)) {
     $table_name = $row['TABLE_NAME'];
     //Ignore Spinsports_Matchbook, it has not been implemented yet, and might never
-    if ($table_name == "Spinsports_Matchbook") {
-        continue;
-    }
     $update_time = strtotime($row['UPDATE_TIME']);
     $now = time();
-    $diff = round(($now - $update_time)/60); //Difference in time in minutes
+    $diff = $now - $update_time; //Difference in time in seconds
     if ($diff <= $green_time) {
         $status = "🟢";
+        $diff = gmdate("i:s", $diff);
     } elseif ($diff <= $orange_time) {
         $status = "🟠";
-    } else {
+        $diff = gmdate("i:s", $diff);
+    } elseif ($diff > $timeout_time) {
         $diff = "∞";
+        $status = "❓";
+    } else {
+        $status = "🔴";
+        $diff = "∞";
+    }
+    if ($table_name == "Spinsports_Matchbook") {
         $status = "🔴";
     }
-    echo "<tr><th>$table_name</th><th>$diff:00</th><th>$status</th></tr>";
+    if (strpos($table_name, "Matchbook")) {
+        echo "<th>$status</th></tr>";
+    } else {
+        echo "<tr><th>$table_name</th><th>$diff</th><th>$status</th>";
+    }
 }
 ?>
 </table>
